677e365e558bebe6a0188b4be69afde0f5989522
gourav <gvirus21yt@gmail.com>
2022-02-28 17:07:32 +0530

    added setIsPublicMintEnabled, setBaseTokenUri, tokenURI & withdraw function

--- contracts/RoboPunkNFT.sol
+++ contracts/RoboPunkNFT.sol
@@ -9,6 +9,38 @@ contract RoboPunksNFT is ERC721, Ownable {
     uint256 public mintPrice;
     uint256 public totalSupply;
     uint256 public maxSupply;
-    
+    uint256 public maxPerWallet;
+    bool public isPublicMintEnabled;
+    string internal baseTokenUri;
+
+    address payable public withdrawWallet;
+    mapping(address => uint256) public walletMints;
+
+    constructor() payable ERC721('RoboPunks', 'RP'){
+        mintPrice = 0.02 ether;
+        totalSupply = 0;
+        maxSupply = 1000;
+        maxPerWallet = 3;
+        withdrawWallet = payable(0x4B1865B7786c517B2dfe6f8C4727320FF5178e6c);
+    }
+
+    function setIsPublicMintEnabled(bool _isPublicMintEnabled ) external onlyOwner {
+        isPublicMintEnabled = _isPublicMintEnabled;
+    }
+
+    function setBaseTokenUri(string calldata _baseTokenUri) external onlyOwner {
+        baseTokenUri = _baseTokenUri;
+    }
+
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        require(_exists(_tokenId), "Token doesn't exist");
+        return string(abi.encodePacked(baseTokenUri, Strings.toString(_tokenId), ".json"));
+    }
+
+    function withdraw() external onlyOwner {
+        (bool sucsess, ) = withdrawWallet.call{value: address(this).balance }('');
+
+        require(sucsess, 'withdraw failed');
+    }
 }
 
